a6311edcc7dc5f586e06c7cfd50eac2d48b6c37a
jeong_jiun <jeong_jiun@naver.com>
2023-12-30 00:12:47 +0900

    init commit

---
+++ src/main/java/com/example/springbootdb_connect/SpringBootDbConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdb_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDbConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDbConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootdb_connect/SpringBootDbConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdb_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDbConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
