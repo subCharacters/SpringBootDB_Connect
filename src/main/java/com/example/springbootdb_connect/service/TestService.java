3e7e9fe9fd732bd0aa10b381cc7bc84d88cc6391
jeong_jiun <jeong_jiun@naver.com>
2024-01-04 11:01:41 +0900

    only jpa

--- src/main/java/com/example/springbootdb_connect/service/TestService.java
+++ src/main/java/com/example/springbootdb_connect/service/TestService.java
@@ -2,7 +2,6 @@ package com.example.springbootdb_connect.service;
 
 import com.example.springbootdb_connect.dto.MembersDTO;
 import com.example.springbootdb_connect.entity.Members;
-import com.example.springbootdb_connect.mapper.MemberMapper;
 import com.example.springbootdb_connect.repository.TestRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,28 +9,16 @@ import org.springframework.stereotype.Service;
 public class TestService {
 
     private TestRepository testRepository;
-    private MemberMapper memberMapper;
 
-    TestService(TestRepository testRepository, MemberMapper memberMapper) {
+    TestService(TestRepository testRepository) {
         this.testRepository = testRepository;
-        this.memberMapper = memberMapper;
     }
     public String getDate() {
         return "20231230";
     }
 
-//    public MembersDTO getMember(String username) {
-//        Members member = testRepository.findByUsername(username);
-//        MembersDTO membersDTO = MembersDTO.builder()
-//                .username(member.getUsername())
-//                .password(member.getPassword())
-//                .nickname(member.getNickname())
-//                .build();
-//        return membersDTO;
-//    }
-
     public MembersDTO getMember(String username) {
-        Members member = memberMapper.findByUsername(username);
+        Members member = testRepository.findByUsername(username);
         MembersDTO membersDTO = MembersDTO.builder()
                 .username(member.getUsername())
                 .password(member.getPassword())
@@ -39,4 +26,5 @@ public class TestService {
                 .build();
         return membersDTO;
     }
+
 }
